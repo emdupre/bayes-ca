8c8403094781457573a23547b61dd3c609f2daee
Elizabeth DuPre <emd222@cornell.edu>
2024-02-12 14:25:47 -0800

    Add schematic four

--- bayes_ca/docs/schematics.py
+++ bayes_ca/docs/schematics.py
@@ -148,7 +148,7 @@ for label, ax in axs.items():
     ax.yaxis.set_tick_params(labelleft=False)
     ax.set_yticks([])
     ax.set_xticks(jnp.arange(1, 16, 2))
-    ax.tick_params(axis="x", direction="in", pad=-17.0)
+    ax.tick_params(axis="x", direction="in", pad=-1.0)
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.spines["bottom"].set_position("zero")
@@ -266,3 +266,144 @@ img.set_clip_path(polygon)
 
 plt.tight_layout()
 plt.show()
+
+
+#######################################
+#### FOURTH SCHEMATIC FIGURE
+#######################################
+
+# set hyper params
+key = jr.PRNGKey(0)
+sigmasq_obs = 0.15**2
+
+# set underlying means and generate observations for first subplot
+true_means = jnp.concatenate(
+    (
+        jnp.ones((5, 1)) * -0.40,
+        jnp.ones((7, 1)) * 0.30,
+        jnp.ones((3, 1)) * 0.80,
+    )
+)
+
+# set third sub-plot
+this_key, key = jr.split(key)
+signal_three = jnp.concatenate(
+    (
+        jnp.ones((12, 1)) * -0.05,
+        jnp.ones((3, 1)) * 0.80,
+    )
+)
+obs = tfd.Normal(signal_three, jnp.sqrt(sigmasq_obs)).sample(seed=key)
+
+# generate line segments for time, run lengths in second subplot
+time = jnp.arange(1, 16)
+run_lengths = jnp.concatenate([jnp.arange(5), jnp.arange(7), jnp.arange(3)])
+rl_coords = [(x, y) for x, y in zip(time, run_lengths)]
+
+# figure generation ; set sizing and x-, y-lims
+fig, axs = plt.subplot_mosaic([["B"], ["C"], ["D"]], layout="constrained", sharex=True, height_ratios=[1, 1, 2], figsize=(7, 8))
+fig.supxlabel("$t$", fontsize="x-large", ha="right", x=1.0)
+
+
+for label, ax in axs.items():
+    # label physical distance to the left and up:
+    trans = mtransforms.ScaledTranslation(-40 / 72, 7 / 72, fig.dpi_scale_trans)
+    ax.text(
+        0.0,
+        1.0,
+        label,
+        transform=ax.transAxes + trans,
+        fontsize="xx-large",
+        va="bottom",
+        fontfamily="sans-serif",
+        fontweight="bold",
+    )
+    ax.tick_params(axis="x", direction="in", pad=-15.0)
+
+
+ax1 = axs["B"]
+ax2 = axs["C"]
+ax3 = axs["D"]
+# fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(7, 5), sharex=True)
+plt.setp(ax1, ylim=(-0.1, 6.5))
+plt.setp(ax2, xlim=(0.75, 15.5), ylim=(-1.0, 1.0))
+plt.setp(ax3, xlim=(0.75, 15.5), ylim=(-1.0, 1.0))
+
+# set first sub-plot
+linestyle = [
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    (5, (10, 3)),
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    (5, (10, 3)),
+    "solid",
+    "solid",
+]
+lc = mc.LineCollection(
+    itertools.pairwise(rl_coords), color="black", linestyle=linestyle, linewidth=0.75
+)
+ax1.set_xticks(jnp.arange(1, 16, 2))
+ax1.set_ylabel(ylabel="$z_t$", labelpad=5.0, fontsize="x-large", rotation="horizontal")
+ax1.add_collection(lc)
+ax1.scatter(time, run_lengths, color="black", marker=".")
+ax1.yaxis.set_label_coords(-0.07, 0.85)
+ax1.spines["top"].set_visible(False)
+ax1.spines["right"].set_visible(False)
+ax1.plot(1, -0.1, ">k", transform=ax1.get_yaxis_transform(), clip_on=False)
+ax1.plot(0.75, 1, "^k", transform=ax1.get_xaxis_transform(), clip_on=False)
+
+# set second sub-plot
+# ax2.plot(jnp.arange(1, 16), obs, "o", alpha=1, color="dimgray")
+lc = mc.LineCollection(
+    [((1, -0.4), (5, -0.4)), ((6, 0.3), (12, 0.3)), ((13, 0.8), (15, 0.8))],
+    color="black",
+    linestyle="dotted",
+)
+ax2.add_collection(lc)
+ax2.axvline(x=5.5, color="black", linestyle=(5, (10, 3)), linewidth=0.75)
+ax2.axvline(x=12.5, color="black", linestyle=(5, (10, 3)), linewidth=0.75)
+ax2.set_ylabel(ylabel="$\mu^0_t$", labelpad=5.0, fontsize="x-large", rotation="horizontal")
+ax2.yaxis.set_tick_params(labelleft=False)
+ax2.set_yticks([])
+ax2.yaxis.set_label_coords(-0.07, 0.85)
+ax2.spines["top"].set_visible(False)
+ax2.spines["right"].set_visible(False)
+ax2.spines["bottom"].set_position("zero")
+ax2.plot(1, 0, ">k", transform=ax2.get_yaxis_transform(), clip_on=False)
+ax2.plot(0.75, 1, "^k", transform=ax2.get_xaxis_transform(), clip_on=False)
+ax2.plot(0.75, 0, "vk", transform=ax2.get_xaxis_transform(), clip_on=False)
+
+
+# set third sub-plot
+ax3.plot(jnp.arange(1, 16), obs, "o", alpha=1, color="dimgray")
+lc = mc.LineCollection(
+    [((1, -0.05), (12, -0.05)), ((13, 0.8), (15, 0.8))],
+    color="black",
+    linestyle="dotted",
+)
+ax3.add_collection(lc)
+# ax3.axvline(x=5.5, color="black", linestyle=(5, (10, 3)), linewidth=0.75)
+ax3.axvline(x=12.5, color="black", linestyle=(5, (10, 3)), linewidth=0.75)
+ax3.set_ylabel(ylabel="$y_t$", labelpad=5.0, fontsize="x-large", rotation="horizontal")
+ax3.yaxis.set_tick_params(labelleft=False)
+ax3.set_yticks([])
+ax3.yaxis.set_label_coords(-0.07, 0.85)
+ax3.spines["top"].set_visible(False)
+ax3.spines["right"].set_visible(False)
+ax3.spines["bottom"].set_position("zero")
+ax3.plot(1, 0, ">k", transform=ax3.get_yaxis_transform(), clip_on=False)
+ax3.plot(0.75, 1, "^k", transform=ax3.get_xaxis_transform(), clip_on=False)
+ax3.plot(0.75, 0, "vk", transform=ax3.get_xaxis_transform(), clip_on=False)
+
+
+# render and show
+# plt.tight_layout()
+# plt.show()
+plt.savefig('schematic-four.svg')
